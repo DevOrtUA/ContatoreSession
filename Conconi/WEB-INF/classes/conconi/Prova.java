b2eec48db87b6b4e00babc0613e82ac2de149620
DevOrtUA <57683132+DevOrtUA@users.noreply.github.com>
2020-01-17 16:57:38 +0100

    fixed

--- Conconi/WEB-INF/classes/conconi/Prova.java
+++ Conconi/WEB-INF/classes/conconi/Prova.java
@@ -7,27 +7,23 @@ import javax.servlet.http.*;
 
 public class Prova extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws IOException, ServletException
-    {
-		Integer x;
-		HttpSession session = request.getSession();
-		PrintWriter out = response.getWriter();
-		
-		if(session.isNew()){
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+	Integer x;
+	HttpSession session = request.getSession();
+	PrintWriter out = response.getWriter();
 
-			session.putValue("contatore", 1);
+	if(session.isNew()){
+		x =new Integer(1);
+		session.putValue("contatore", x);
 
-			x =new Integer(1);
-
-		}else{
-
-			x =(Integer)session.getValue("contatore");
-
-			session.putValue("contatore", new Integer(x.intValue()+1));
-			
-			
-    }
-	out.println("Access n: "+x);
 	}
+
+	x =(Integer)session.getValue("contatore");
+
+	session.putValue("contatore", new Integer(x.intValue()+1));
+			
+			
+    
+	out.println("Access n: "+x);
+    }
 }
